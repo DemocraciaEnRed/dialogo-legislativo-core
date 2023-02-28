50d8d2f990cbdfb63c5565ec8f212d3d38a54c9b
Mariano Valdez <marianovaldez92@protonmail.com>
2023-02-28 13:29:54 -0300

    docker & run dev

--- run-dev.sh
+++ run-dev.sh
@@ -1,3 +1,1 @@
-docker ps -a | grep mongo3.6 | docker start mongo3.6 || docker run -d --name mongo3.6 -p 27017:27017 -v `pwd`/tmp/db:/data/db -w /data/db mongo:3.6
-
-npm run start
+docker-compose up
