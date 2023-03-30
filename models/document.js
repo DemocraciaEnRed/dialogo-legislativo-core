52da092ccd5cceb702ae425ade75c1fbf987677d
Guillermo Croppi <guillermocroppi@gmail.com>
2023-03-30 15:08:11 -0300

    Added emote data for document model

--- models/document.js
+++ models/document.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose')
 const mongoosePaginate = require('mongoose-paginate')
 
-
 const ApoyoSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false },
-  email: { type: String},
+  email: { type: String },
   nombreApellido: String,
   fecha: { type: Date, default: Date.now }
 })
@@ -17,7 +16,11 @@ const Document = new mongoose.Schema({
   published: { type: Boolean, required: true, default: false },
   publishedMailSent: { type: Boolean },
   commentsCount: { type: Number, default: 0 },
-  apoyos: [ApoyoSchema]
+  apoyos: [ApoyoSchema],
+  emoteLike: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', default: [] }],
+  emoteLove: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', default: [] }],
+  emoteImprove: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', default: [] }],
+  emoteDislike: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', default: [] }]
 }, {
   timestamps: true
 })
@@ -25,7 +28,17 @@ const Document = new mongoose.Schema({
 // Model's Plugin Extensions
 Document.plugin(mongoosePaginate)
 
-Document.virtual('apoyosCount').get(() => this.apoyos && this.apoyos.length || 0 )
+Document.virtual('apoyosCount').get(() => this.apoyos && this.apoyos.length || 0)
+
+Document.virtual('emotesCount').get(() => {
+  return {
+    likes: (this.emoteLike && this.emoteLike.length) || 0,
+    loves: (this.emoteLove && this.emoteLove.length) || 0,
+    improve: (this.emoteImprove && this.emoteImprove.length) || 0,
+    dislike: (this.emoteDislike && this.emoteDislike.length) || 0,
+    total: ((this.emoteLike && this.emoteLike.length) || 0) + ((this.emoteLove && this.emoteLove.length) || 0) + ((this.emoteImprove && this.emoteImprove.length) || 0) + ((this.emoteDislike && this.emoteDislike.length) || 0)
+  }
+})
 
 // Expose Model
 module.exports = mongoose.model('Document', Document)
